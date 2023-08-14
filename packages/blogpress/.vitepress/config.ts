239b090a9b3ae2ec599cf182d447c269dcd04311
xufuying <xufuying@100doc.com.cn>
2023-08-14 15:46:46 +0800

    同步掘金的文章

--- packages/blogpress/.vitepress/config.ts
+++ packages/blogpress/.vitepress/config.ts
@@ -119,15 +119,52 @@ export default defineConfig({
   themeConfig: {
     search: {
       provider: 'algolia',
-      // provider: "local"
       options: {
         appId: 'FSCXD4L73U',
         apiKey: 'b3699c5743c4e01b66a48e52b386a206',
         indexName: 'xxytime',
         placeholder: '请输入关键词',
-        translations: {
-          button: {
-            buttonText: '搜索文档'
+        locales: {
+          zh: {
+            placeholder: '搜索文档',
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消'
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除'
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接'
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                  searchByText: '搜索提供者'
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈'
+                }
+              }
+            }
           }
         }
       }
