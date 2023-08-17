66569cb7982585d25b2b70c3aa19ed22ed276463
xufuying <xufuying@100doc.com.cn>
2023-08-17 18:02:52 +0800

    update

--- packages/blogpress/.vitepress/config.ts
+++ packages/blogpress/.vitepress/config.ts
@@ -172,7 +172,7 @@ export default defineConfig({
     lastUpdatedText: '上次更新于',
     footer: {
       message:
-        '<a style="display:inline-block;" target="_blank" href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral/"><img width="80" src="/又拍云_logo2.png"/></a>',
+        '<a style="display:inline-block;" target="_blank" href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral"><img width="80" src="/又拍云_logo2.png"/></a>',
       copyright: `<a target="_blank" href="https://beian.miit.gov.cn/">皖ICP备18011786号-1</a> © 2020-present 小小荧`
     },
     logo: '/logo.png',
